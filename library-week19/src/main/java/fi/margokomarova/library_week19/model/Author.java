a2f047beb499c4b50b57a2991526764507dca4dd
marrgosha <marrgosha4@gmail.com>
2025-04-27 14:58:20 +0300

    feat:entities created

--- library-week19/src/main/java/fi/margokomarova/library_week19/model/Author.java
+++ library-week19/src/main/java/fi/margokomarova/library_week19/model/Author.java
@@ -1,4 +1,30 @@
 package fi.margokomarova.library_week19.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
 }
+
+
--- library-week19/src/main/java/fi/margokomarova/library_week19/model/Book.java
+++ library-week19/src/main/java/fi/margokomarova/library_week19/model/Book.java
@@ -1,4 +1,36 @@
 package fi.margokomarova.library_week19.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @ManyToMany
+    @JoinTable(
+            name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id")
+    )
+    private Set<Author> authors;
+
 }
+
+
